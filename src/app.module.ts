13e4757a85edca6cb4d2e94b8d3f86ad0ff79a56
mastho19 <thom.almeida19@gmail.com>
2022-11-01 16:17:09 -0300

    tudo certo

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,27 +12,27 @@ import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    //   TypeOrmModule.forRoot({
-    //   type: "mysql", 
-    //   host: "localhost",
-    //   port: 3306,
-    //   username: "root",
-    //   password: "root",
-    //   database: "ecommerce",
-    //   entities: [Produto, Categoria, Usuario],
-    //   synchronize: true,
-    // }),
-     TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      logging: false,
-      dropSchema: false,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      TypeOrmModule.forRoot({
+      type: "mysql", 
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "root",
+      database: "db_ecommerce",
+      entities: [Produto, Categoria, Usuario],
       synchronize: true,
-      autoLoadEntities: true,
-    }), 
+    }),
+    //  TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   url: process.env.DATABASE_URL,
+    //   logging: false,
+    //   dropSchema: false,
+    //   ssl: {
+    //     rejectUnauthorized: false,
+    //   },
+    //   synchronize: true,
+    //   autoLoadEntities: true,
+    // }), 
     ProdutoModule, CategoriaModule, UsuarioModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
